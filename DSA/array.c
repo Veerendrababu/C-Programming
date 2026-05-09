e64303c929d0fcbe9eaf68a50c9ee9de95710f8f
VeerendraBabu <b.veerendra98@gmail.com>
2026-05-09 19:10:24 +0530

    Updated code base

---
+++ DSA/array.c
@@ -0,0 +1,12 @@
+//
+//  array.c
+//  
+//
+//  Created by Veerendra Babu Bommakanti on 5/9/26.
+//
+#include<stdio.h>
+
+int main(){
+    int array[10] = {0};
+    printf("Array[2] : %d\n",array[2]);
+}
